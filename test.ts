2c69ae3a48a0649538b05096eb7282ca065ad93b
Michael <hanyaonian@outlook.com>
2021-01-28 08:51:32 +0800

    Update test.ts
    update testcase. (due to pos changing)

--- test.ts
+++ test.ts
@@ -5,17 +5,17 @@ const testCase = [
 		key: ['her', 'he', 'she', 'hers'],
 		text: "ushers",
 		expect: [
-			{ pos: [1, 3], word: 'she' },
-			{ pos: [2, 5], word: 'hers' },
-			{ pos: [2, 3], word: 'he' },
-			{ pos: [2, 4], word: 'her'},
+			{ pos: 3, word: 'she' },
+			{ pos: 5, word: 'hers' },
+			{ pos: 3, word: 'he' },
+			{ pos: 4, word: 'her'},
 		]
 	}
 ]
 
 for (let i = 0; i < testCase.length; i++) {
 	let th = new textHelper(testCase[i].key);
-	const response = th.searchWord(testCase[i].text);
+	const response = th.search(testCase[i].text);
 	console.log('out', response.sort());
 	console.log('expected', testCase[i].expect.sort());
 }
