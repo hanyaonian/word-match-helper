afdac49f8a13d6ff8a2b5c61a9320583d71503a6
Michael <hanyaonian@outlook.com>
2021-01-28 15:00:35 +0800

    Update test.ts

--- test.ts
+++ test.ts
@@ -14,17 +14,19 @@ const testCase = [
 	// default regexp: skip all space
 	{
 		reg: /\s+|\*/g,
-        key: ['shit'],
-        text: "s h ** i t",
-        expect: [
-            { pos: 9, word: 'shit' },
-        ]
-    }
+		key: ['shit'],
+		text: "s h ** i t",
+		expect: [
+		    { pos: 9, word: 'shit' }
+		]
+    	}
 ]
 
 for (let i = 0; i < testCase.length; i++) {
 	let th = new textHelper(testCase[i].key, testCase[i].reg);
 	const response = th.search(testCase[i].text);
-	console.log('out', response.sort());
-	console.log('expected', testCase[i].expect.sort());
+	console.log('word:', testCase[i].key, 'text:', testCase[i].text);
+	console.log('match:', response.sort());
+	console.log('expect:', testCase[i].expect.sort());
+	console.log('filtered:', th.filter(testCase[i].text))
 }
