2b5b76126b12789403da031597895136c01cc852
MichaelHyn <hanyaonian@outlook.com>
2022-12-30 00:07:19 +0800

    chore: :tada: remove filter; update version

--- README.md
+++ README.md
@@ -1,73 +1,36 @@
 # word-match-helper
 
-Aho–Corasick based word checking. Match & filter words.
+Aho–Corasick based word checking. Match words fast.
 
-## install
+## Installation
 
 ````sh
 npm install word-match-helper
 ````
 
+## development & test
+
+```sh
+npm run build
+npm run dev
+npm run test # basic cases 
+```
+
 ## Basic Usage
 
 ````javascript
-import checker from 'word-match-helper';
+import { WordMatcher } from 'word-match-helper';
 
-const wordChecker = new checker({
+const wordChecker = new WordMatcher({
   targets: ['her', 'he', 'hers'],
 });
+
+// excepted matched result: ['her', 'he', 'hers']
 wordChecker.search('ushers');
-/* excepted:
-*  [
-*    { pos: 5, word: 'hers' },
-*    { pos: 4, word: 'he' },
-*    { pos: 5, word: 'her' }
-*  ]
-*/
 
 // add additional word list 
 wordChecker.addWord(['she'])
+
+// excepted matched result: ['her', 'he', 'hers', she]
 wordChecker.search('ushers');
-/* excepted:
-*  [
-     { pos: 4, word: 'she' },
-*    { pos: 6, word: 'hers' },
-*    { pos: 4, word: 'he' },
-*    { pos: 5, word: 'her' }
-*  ]
-*/
-````
-
-### extra options
-
-- `ignorePatt`: Regexpression. default is /\0/g, match null.
-- `baseStrict`: Boolean. default is false. If false, 'App' match 'app', if true, 'App' won't mathc 'app'
-
-#### skip special character
-
-````js
-// pass customize reg expression to skip confusion charater
-// e.g skip spaces and '*'
-const wordChecker = new checker({
-  targets: ['shit'],
-  ignorePatt: /\s+|\*/g
-});
-wordChecker.search('13213 s h ** i t');
-/* excepted:
-*   [{ pos: 16, word: 'shit' }]
-*/
-````
-
-#### block word
-
-````js
-const wordChecker = new checker({
-  ignorePatt: /\s+|\*/g,
-  baseStrict: true,
-  targets: ['shit'],
-});
-wordChecker.filter('13213 s h ** i t', '*');
-/* excecpted
-*  '13213 *********'
-*/
 ````
--- src/trees.ts
+++ src/trees.ts
@@ -1,17 +1,12 @@
-import { ACTreeContainer, ACTreeNode, SearchResult, Options } from './types';
-import { Node } from './node';
-import { arrayDeduplication } from './utils';
+import { arrayDeduplication } from "./utils";
+import { Node } from "./node";
 
 export default class AhoCorasick {
   private root: Node;
   public currentState: number = 0;
   public wordset: Set<string> = new Set();
 
-  // \0 means match null (U+0000)
-  private ignorePatt: RegExp = /\0/g;
-  private toLowerCase: boolean = true;
-
-  constructor(config: Options) {
+  constructor(config: { targets: string[] }) {
     const { targets } = config;
 
     // initiate root node
@@ -28,17 +23,17 @@ export default class AhoCorasick {
 
   private initACStateTree(wordlist: string[]): void {
     const words = arrayDeduplication(wordlist);
-    words.forEach(word => {
+    words.forEach((word) => {
       let currentNode = this.root;
       let len = word.length;
       for (let i = 0; i < len; i++) {
-        const next = currentNode.setChildren({
+        const child = currentNode.setChildren({
           char: word[i],
           root: this.root,
           is_match: i === len - 1, // state match words end
         });
         // get next level
-        currentNode = next;
+        currentNode = child;
       }
       // todo: fix this
       currentNode.isMatch = true;
@@ -49,13 +44,13 @@ export default class AhoCorasick {
 
   /**
    * @param root tireTree root
-   * 
-   * how to get each node's backNode: 
+   *
+   * how to get each node's backNode:
    * 1. if node is root, backnode points to itself
    * 2. if parents are root, backnode points to root
    * 3. find childNode of parentNode's backNode, if the character value are same,
    *    then that childNode is this node's backNode
-   * 4. (loop) childNode of parentNode's backNode doesn't fit current node, 
+   * 4. (loop) childNode of parentNode's backNode doesn't fit current node,
    *    continue to find parentNode's parentNode
    */
   private setBackNode(node: Node): void {
@@ -72,7 +67,7 @@ export default class AhoCorasick {
         }
         let backNode = parentNode.backNode;
         while (backNode) {
-          let child = backNode.children[currentNode.char || ''];
+          let child = backNode.children[currentNode.char || ""];
           if (child) {
             currentNode.backNode = child;
             break;
@@ -87,18 +82,13 @@ export default class AhoCorasick {
 
   /**
    * @param text search text
-   * 
+   *
    */
-  public search(text: string): SearchResult[] {
-    text = this.toLowerCase ? text.toLocaleLowerCase() : text;
+  public search(text: string) {
     let words = [];
     let currentNode = this.root;
     for (let i = 0; i < text.length; i++) {
       let char = text[i];
-      // skip
-      if (char.match(this.ignorePatt)) {
-        continue;
-      }
       let child = currentNode.children[char];
       if (!child) {
         // if doesn't match, find backNode until get root.
@@ -118,8 +108,8 @@ export default class AhoCorasick {
           if (backNode.isMatch) {
             words.push({
               pos: i + 1,
-              word: this.getWord(backNode)
-            })
+              word: this.getWord(backNode),
+            });
           }
           backNode = backNode.backNode as Node;
         }
@@ -131,50 +121,11 @@ export default class AhoCorasick {
     return words;
   }
 
-  /**
-   * @param text target text
-   * @param replacement default '*'
-   * @return text replace matching word to replacement
-   * ['he'], 'she' -> 's**'
-   */
-  public filter(text: string, replacement: string = '*'): string {
-    let wordArr = text.split('');
-    let skipWordMap = new Map<number, string>();
-    let matchedWord = this.search(text);
-    matchedWord.sort((pre, next) => pre.pos - next.pos);
-    for (let i = 0; i < matchedWord.length; i++) {
-      let currentWord = matchedWord[i].word;
-      let currentPos = matchedWord[i].pos;
-      let lword = skipWordMap.get(currentPos);
-      if (lword && lword.length < currentWord.length) {
-        // get same position's longest word
-        skipWordMap.set(currentPos, currentWord)
-      }
-      if (!lword) {
-        skipWordMap.set(currentPos, currentWord)
-      }
-    }
-    skipWordMap.forEach((word, pos) => {
-      let i = word.length - 1;
-      let moved = 0;
-      while (i >= 0) {
-        if (word[i] === wordArr[pos - moved] ||
-          (wordArr[pos - moved] === replacement && !wordArr[pos - moved].match(this.ignorePatt))
-        ) {
-          i -= 1;
-        }
-        moved += 1;
-      }
-      wordArr.splice(pos - moved + 1, moved, ...new Array(moved).fill(replacement));
-    })
-    return wordArr.join('');
-  }
-
   private getWord(node: Node): string {
-    let word = '';
+    let word = "";
     while (node.parent && node.char) {
       word = node.char + word;
-      node = node.parent
+      node = node.parent;
     }
     return word;
   }
